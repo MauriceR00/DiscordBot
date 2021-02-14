8fbfcedfb73ed90ec4d14153c86981d693dd7ab9
MauriceR00 <ruble.maurice@web.de>
2021-02-14 17:16:26 +0100

    master

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const csgostatus = require('./methods/csgostatus');
-const prefix = "!";
+const writelog = require('./methods/writelog');
 
+
+const prefix = "!";
 const channelid = "806520416104873994";
 const steamkey = "BEDE9C3683E125B0C57AB65B5D8E0427";
 const faceitkey = "c80a34ed-820e-4cf8-a596-b3825dd66575";
-const botkey = "ODA3NjYxMDc0NzEwNzkwMTQ2.YB7O_Q.LnO6cY4D-2CQGgYyHlT-sAa0bd8";
+var botkey;
+
+const dckey = './key.txt';
+var dcread = fs.readFileSync(dckey);
+if(!dcread) {
+    writelog(`Cant get Discord Bot Key!`);
+    return console.log(`Cant get Discord Bot Key!`);
+}
+botkey = dcread.toString();
 
 const path = './data.json';
 var read = fs.readFileSync(path);
