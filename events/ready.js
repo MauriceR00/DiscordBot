8ca0b16ce5f6d117a9a720078afa0a117fde68b9
MauriceR00 <ruble.maurice@web.de>
2021-02-14 16:18:14 +0100

    master

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const writelog = require('../methods/writelog');
+
 module.exports = (client) => {
+    writelog(`Logged in as ${client.user.tag}`);
     console.log(`Logged in as ${client.user.tag}`);
 }
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const prefix = "!";
 const channelid = "806520416104873994";
 const steamkey = "BEDE9C3683E125B0C57AB65B5D8E0427";
 const faceitkey = "c80a34ed-820e-4cf8-a596-b3825dd66575";
+const botkey = "ODA3NjYxMDc0NzEwNzkwMTQ2.YB7O_Q.LnO6cY4D-2CQGgYyHlT-sAa0bd8";
 
 const path = './data.json';
 var read = fs.readFileSync(path);
@@ -26,6 +27,7 @@ exports.discord = Discord;
 exports.path = path;
 exports.client = client;
 exports.channelid = channelid;
+exports.botkey = botkey;
 
 fs.readdir("./events/", (err, files) => {
     files.forEach((file) => {
@@ -35,4 +37,4 @@ fs.readdir("./events/", (err, files) => {
     })
 })
 
-client.login("ODA3NjYxMDc0NzEwNzkwMTQ2.YB7O_Q.LnO6cY4D-2CQGgYyHlT-sAa0bd8");
+client.login(botkey);
