4947be8b0bf57bcbb0a86cfc9ee62931773bd01a
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-23 21:09:24 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,18 +1,16 @@
-const logout = require("../commands/logout");
-const server = require("../commands/server");
-const ping = require("../commands/ping");
-const steamid = require("../commands/steamid");
-const csgo = require("../commands/csgo");
-const faceit = require("../commands/faceit");
 const writelog = require('../methods/writelog');
-const axios = require('axios');
-const setsteamid = require("../commands/setsteamid");
-const clear = require("../commands/clear");
-const prefix = "!";
+const csgo = require('../commands/csgo');
+const steamid = require('../commands/steamid');
+const removesteamid = require('../commands/removesteamid');
+const setsteamid = require('../commands/setsteamid');
+const faceit = require('../commands/faceit');
+const { Channel } = require('discord.js');
+const prefix = '!';
 
 module.exports = (client, msg) => {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
+    if(msg.author.bot) return;
+    if(!msg.guild) return;
+    if(!msg.content.startsWith(prefix)) return;
 
     const commandBody = msg.content.slice(prefix.length);
     const args = commandBody.split(' ');
@@ -20,12 +18,9 @@ module.exports = (client, msg) => {
 
     writelog(`"${msg.author.username}" -> ${command} (${args})`);
 
-    if(command === "logout") return logout(msg);
-    if(command === "server") return server(msg);
-    if(command === "ping") return ping(msg);
-    if(command === "steamid") return steamid(msg, args);
     if(command === "csgo") return csgo(msg);
-    if(command === "faceit") return faceit(msg);
+    if(command === "steamid") return steamid(msg, args);
+    if(command === "removesteamid") return removesteamid(msg);
     if(command === "setsteamid") return setsteamid(msg, args);
-    if(command === "clear") return clear(msg, args);
+    if(command === "faceit") return faceit(msg);
 }
