7185c989d88f8c2aee6429c4663dda96eb2dc209
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-23 21:08:46 +0100

    Update setsteamid.js

--- commands/setsteamid.js
+++ commands/setsteamid.js
@@ -1,53 +1,38 @@
-const fetch = require('node-fetch');
 const index = require('../index');
 const writelog = require('../methods/writelog');
 const setPresence = require('../methods/setPresence');
 
 module.exports = async (msg, id) => {
+    let steam = index.steam;
+    let faceit = index.faceapi;
     const member = msg.member;
     const channel = msg.channel;
+    const uid = member.id; 
+    
+    //DISCORDID STEAMID
 
-    //USAGE: !setsteamid DISCORD_USER STEAMID
-
-    if(channel.id !== index.channelid) return;
     if(member.id !== "335926647834542081") return;
-
     if(id.length < 2) {
-        msg.delete();
-        return;
+        return channel.send(`Bitte gib eine gültige SteamID oder Steam Profil URL an!`);
     }
-    let usrid = id[0];
-    let sid = id[1];
+    if(index.file[id[0]]) {
+        writelog(`${member.user.username} versuchte einen bereits verbundenen Account erneut zu verbinden!`);
+        return channel.send(`Du hast bereits einen Steam Account zu deinen Discord Account verbunden!`);
+    }
 
+    setPresence(`🦊 Suche SteamID...`);
     msg.channel.startTyping();
-    setPresence("Sammle Informationen...", "WATCHING");
 
-    if(index.file[usrid]) {
-        msg.delete();
-        writelog(`${member.user.username} versuchte bereits gebundenen Account zu binden!`);
-        return channel.send(`Die SteamID ist bereits zum Discord account gebunden! (${index.file[usrid].sid} und ${index.file[usrid].fic})`);
-    }
-    if(id === undefined) return;
+    let steamid = await steam.resolve(id[1]).then(p => { return p; } ).catch(err => { writelog(`ERROR: ${err}`); channel.send(`ERROR: ${err}`) });
+    let username = await steam.getUserSummary(steamid).then(p => { return p.nickname; } ).catch(err => { writelog(`ERROR: ${err}`); channel.send(`ERROR: ${err}`) });
+    let faceitid = await faceit.pl(steamid).then(data => { return JSON.stringify(data.player_id).replaceAll('"', '')}).catch(err => { writelog(`${err}`); channel.send(`${err}`) });
 
-    const result = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${index.steamkey}&steamids=${sid}`).then(sres => sres.json());
-    if(result === undefined) {
-        writelog(`Konnte keine SteamID unter ${sid} finden! (${member.user.username})`);
-        return channel.send(`Keine SteamID unter ${sid} gefunden!`);
+    if (steamid !== undefined) {
+        index.file[uid] = { sid: steamid, fic: faceitid };
+        index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
+        setPresence(`FuXTrupp 🦊`, `WATCHING`);
+        msg.channel.stopTyping();
+        return channel.send(`Steam Account ${username} mit SteamID ${steamid} mit den Discord Account ${id[0]} verbunden!`);
     }
-    let ids = JSON.stringify(result.response.players[0].steamid).replaceAll('"', '');
-    //console.log(ids);
-    const faceit = await fetch(`https://open.faceit.com/data/v4/players?game=csgo&game_player_id=${ids}`, { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': "Bearer " + index.faceitkey }} ).then(fcres => fcres.json());
-    if(faceit === undefined) {
-        writelog(`Kein FaceIT Account unter ${ids} gefunden! (${member.user.username})`);
-        return channel.send(`Kein FaceIT Account unter ${ids} gefunden!`);
-    }
-    let fcid = JSON.stringify(faceit.player_id).replaceAll('"', '');
-    index.file[usrid] = { sid: ids, fic: fcid };
-    //console.log(fcid);
-    index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
-    let usr = index.client.users.cache.find(u => u.id === usrid);
-    writelog(`SteamID (${ids}) & FaceIT ID (${fcid}) zu Discord ACC von ${usr.username} gebunden!`);
-    msg.channel.stopTyping();
-    setPresence("NABOKI", "WATCHING");
-    return channel.send(`SteamID (${ids}) und FaceIT ID (${fcid}) zum Discord Account von ${usr.username} gebunden!`);
+
 }
