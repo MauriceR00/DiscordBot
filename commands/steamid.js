a4a3cf9dfa72fbc520302b42e5c591e80f4a98e8
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-23 21:08:57 +0100

    Update steamid.js

--- commands/steamid.js
+++ commands/steamid.js
@@ -1,54 +1,52 @@
-const fetch = require('node-fetch');
 const index = require('../index');
 const writelog = require('../methods/writelog');
 const setPresence = require('../methods/setPresence');
 
 module.exports = async (msg, id) => {
+    let steam = index.steam;
+    let faceit = index.faceapi;
     const member = msg.member;
     const channel = msg.channel;
-
-    if(channel.id !== index.channelid) return msg.delete();
+    const uid = member.id; 
+    
+    if(channel.id !== "953378480282828861") return;
 
     if(id.length < 1) {
-        msg.delete();
-        return channel.send(`Bitte gib eine SteamID64 an! Beispiel: **!steamid 76561198880500806**`);
+        return channel.send(`Bitte gib eine gültige SteamID oder Steam Profil URL an!`);
     }
-    if(index.file[member.id]) {
-        msg.delete();
-        writelog(`${member.user.username} versuchte bereits gebundenen Account zu binden!`);
-        return channel.send(`Die SteamID ist bereits zum Discord account gebunden! (${index.file[member.id].sid} und ${index.file[member.id].fic})`);
+    if(index.file[uid]) {
+        writelog(`${member.user.username} versuchte einen bereits verbundenen Account erneut zu verbinden!`);
+        return channel.send(`Du hast bereits einen Steam Account zu deinen Discord Account verbunden!`);
     }
-    if(index.fetchrequest) {
-        msg.delete();
-        return channel.send(`Aktuell läuft eine Abfrage. Bitte warte bis diese Beendet ist!`);
+    if(hasValueDeep(index.file, id[0])) {
+        writelog(`${member.user.username} versuchte einen bereits verbundenen Account erneut zu verbinden!`);
+        return channel.send(`Du hast bereits einen Steam Account zu deinen Discord Account verbunden!`);
     }
-    //let file = require("./data.json");
-    if(id === undefined) return channel.send(`Bitte eine SteamID angeben! Beispiel: **!steamid 76561198880500806**`);
 
-    index.fetchrequest = true;
+    setPresence(`🦊 Suche SteamID...`);
     msg.channel.startTyping();
-    setPresence("Sammle Informationen...", "WATCHING");
-    const result = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${index.steamkey}&steamids=${id}`).then(sres => sres.json());
-    if(result === undefined) {
-        index.fetchrequest = false;
-        writelog(`Konnte keine SteamID unter ${id} finden! (${member.user.username})`);
-        return channel.send(`Keine SteamID unter ${id} gefunden!`);
+
+    let steamid = await steam.resolve(id[0]).then(p => { return p; } ).catch(err => { writelog(`ERROR: ${err}`); channel.send(`ERROR: ${err}`) });
+    let username = await steam.getUserSummary(steamid).then(p => { return p.nickname; } ).catch(err => { writelog(`ERROR: ${err}`); channel.send(`ERROR: ${err}`) });
+    let faceitid = await faceit.pl(steamid).then(data => { return JSON.stringify(data.player_id).replaceAll('"', '')}).catch(err => { writelog(`${err}`); channel.send(`${err}`) });
+
+    if (steamid !== undefined) {
+        index.file[uid] = { sid: steamid, fic: faceitid };
+        index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
+        setPresence(`FuXTrupp 🦊`, `WATCHING`);
+        msg.channel.stopTyping();
+        return channel.send(`Steam Account ${username} mit SteamID ${steamid} mit den Discord Account ${member.user.username} verbunden!`);
     }
-    let ids = JSON.stringify(result.response.players[0].steamid).replaceAll('"', '');
-    //console.log(ids);
-    const faceit = await fetch(`https://open.faceit.com/data/v4/players?game=csgo&game_player_id=${ids}`, { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': "Bearer " + index.faceitkey }} ).then(fcres => fcres.json());
-    if(faceit === undefined) {
-        index.fetchrequest = false;
-        writelog(`Kein FaceIT Account unter ${ids} gefunden! (${member.user.username})`);
-        return channel.send(`Kein FaceIT Account unter ${ids} gefunden!`);
-    }
-    let fcid = JSON.stringify(faceit.player_id).replaceAll('"', '');
-    index.file[member.id] = { sid: ids, fic: fcid };
-    //console.log(fcid);
-    index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
-    writelog(`SteamID (${ids}) & FaceIT ID (${fcid}) zu Discord ACC von ${member.user.username} gebunden!`);
-    index.fetchrequest = false;
-    msg.channel.stopTyping();
-    setPresence("NABOKI", "WATCHING");
-    return channel.send(`SteamID (${ids}) und FaceIT ID (${fcid}) zum Discord Account von ${member.user.username} gebunden!`);
+
+}
+
+function hasValueDeep(json, findValue) {
+    const values = Object.values(json);
+    let hasValue = values.includes(findValue);
+    values.forEach(function(value) {
+        if (typeof value === "object") {
+            hasValue = hasValue || hasValueDeep(value, findValue);
+        }
+    })
+    return hasValue;
 }
