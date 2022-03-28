1b95aa4a375c9224aa5e24abbc70641f393ccc50
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-28 20:40:46 +0200

    Update csgo.js

--- commands/csgo.js
+++ commands/csgo.js
@@ -1,16 +1,18 @@
 const index = require('../index');
 const rtt = require('../methods/roundToTwo');
 const writelog = require('../methods/writelog');
-const setPresence = require('../methods/setPresence');
+const ranks = [ "Unranked", "Silver 1", "Silver 2", "Silver 3", "Silver 4", "Silver Elite", "Silver Elite Master", "Gold Nova 1", "Gold Nova 2", "Gold Nova 3", "Gold Nova Master", "Master Guardian 1", "Master Guardian 2", "Master Guardian Elite", "Distinguished Master Guardian", "Legendary Eagle", "Legendary Eagle Master", "Supreme Master First Class", "The Global Elite"];
+const rankemotes = [ ":unranked:", ":s1:", ":s2:", ":s3:", ":s4:", ":se:", ":sem:", ":gn1:", ":gn2:", ":gn3:", ":gnm:", ":mg1:", "<:mg2:956626888321810482>", ":mge:", ":dmg:", ":le:", ":lem:", ":supreme:", ":globalelite:"];
 
 module.exports = async (msg) => {
     const member = msg.member;
     const channel = msg.channel;
     const id = member.id;
+    const guild_id = msg.guild.id;
     let steam = index.steam;
 
 
-    //if(channel.id !== "953378480282828861") return;
+    if(channel.id !== index.file[guild_id].mainchannel) return;
 
     if(!index.file[id]) {
         writelog(`${member.user.username} hat kein Steam Profil zum Discord Account gebunden!`);
@@ -20,7 +22,6 @@ module.exports = async (msg) => {
     let steamid = index.file[id].sid;
 
     msg.channel.startTyping();
-    setPresence("🦊 Sammle Informationen...", "WATCHING");
 
     let usersummary = await steam.getUserSummary(steamid).then(p => { return p; } ).catch(err => { writelog(`ERROR: ${err}`); });
     let userstats = await steam.getUserStats(steamid, "730").then(p => { return p; } ).catch(err => { writelog(`ERROR: ${err}`); });
@@ -28,15 +29,11 @@ module.exports = async (msg) => {
     kd = rtt(userstats.stats.total_kills / userstats.stats.total_deaths);
     headshotp = rtt((userstats.stats.total_kills_headshot * 100) / userstats.stats.total_kills);
 
-    let asd;
-    await index.csgoi.requestPlayersProfile(steamid, function(data) {
-        asd = data.account_id;
-        //data.ranking.wins
-        //data.ranking.rank_id
-        return console.log(asd);
+	const x = await new Promise(function(resolve, reject) {
+        index.csgoi.requestPlayersProfile(steamid, function(data) {
+            resolve(data)
+        });
     });
-    //console.log(a);
-
 
     let mbd = new index.discord.MessageEmbed()
         .setColor(1848932)
@@ -56,8 +53,14 @@ module.exports = async (msg) => {
         .addField('Shots Hit', userstats.stats.total_shots_hit, true)
         .addField('MVPs', userstats.stats.total_mvps, true);
 
-
-    setPresence(`FuXTrupp 🦊`, `WATCHING`);
+    try {
+        if(x !== undefined) {
+            if(x.ranking.wins !== undefined) mbd.addField("Matchmaking Wins", x.ranking.wins, true);
+            if(x.ranking.rank_id !== undefined) mbd.addField("Matchmaking Rank", ranks[x.ranking.rank_id], true);
+        }
+    } catch(err) {
+        writelog(`ERROR: ${err}`);
+    }
     msg.channel.stopTyping();
     channel.send(mbd);
 }
