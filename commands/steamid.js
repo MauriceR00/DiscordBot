11f7dfbffe5d19af34a5e8b65814f928cdc06fcc
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-28 20:42:17 +0200

    Update steamid.js

--- commands/steamid.js
+++ commands/steamid.js
@@ -1,15 +1,15 @@
 const index = require('../index');
 const writelog = require('../methods/writelog');
-const setPresence = require('../methods/setPresence');
 
 module.exports = async (msg, id) => {
     let steam = index.steam;
     let faceit = index.faceapi;
     const member = msg.member;
     const channel = msg.channel;
+    const guild_id = msg.guild.id;
     const uid = member.id; 
     
-    if(channel.id !== "953378480282828861") return;
+    if(channel.id !== (index.file[guild_id].mainchannel)) return;
 
     if(id.length < 1) {
         return channel.send(`Bitte gib eine gültige SteamID oder Steam Profil URL an!`);
@@ -22,8 +22,7 @@ module.exports = async (msg, id) => {
         writelog(`${member.user.username} versuchte einen bereits verbundenen Account erneut zu verbinden!`);
         return channel.send(`Du hast bereits einen Steam Account zu deinen Discord Account verbunden!`);
     }
-
-    setPresence(`🦊 Suche SteamID...`);
+    
     msg.channel.startTyping();
 
     let steamid = await steam.resolve(id[0]).then(p => { return p; } ).catch(err => { writelog(`ERROR: ${err}`); channel.send(`ERROR: ${err}`) });
@@ -33,7 +32,6 @@ module.exports = async (msg, id) => {
     if (steamid !== undefined) {
         index.file[uid] = { sid: steamid, fic: faceitid };
         index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
-        setPresence(`FuXTrupp 🦊`, `WATCHING`);
         msg.channel.stopTyping();
         return channel.send(`Steam Account ${username} mit SteamID ${steamid} mit den Discord Account ${member.user.username} verbunden!`);
     }
