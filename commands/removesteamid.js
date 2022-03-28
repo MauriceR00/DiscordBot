ecc6a22a5f8ecef2ec16da0ef14e19b557ce2aac
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-28 20:41:23 +0200

    Update removesteamid.js

--- commands/removesteamid.js
+++ commands/removesteamid.js
@@ -1,15 +1,18 @@
 const index = require('../index');
 const writelog = require('../methods/writelog');
-const setPresence = require('../methods/setPresence');
 
 module.exports = async (msg) => {
     const member = msg.member;
     const channel = msg.channel;
+    const guild_id = msg.guild.id;
     const uid = member.id; 
+
+    if(channel.id !== (index.file[guild_id].mainchannel)) return;
 
     if(index.file[uid]) {
         delete index.file[uid];
         index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
+        writelog(`Der verbundene Steam Account von ${member.user.username} wurde entfernt!`);
         channel.send(`Der verbundene Steam Account von ${member.user.username} wurde entfernt!`);
         return;
     } else {
