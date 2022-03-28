d67002e5f59e730ee2ff36c9aaf9223b86fccaca
MauriceR00 <76565838+MauriceR00@users.noreply.github.com>
2022-03-28 20:41:42 +0200

    Add files via upload

---
+++ commands/setbotadmin.js
@@ -0,0 +1,16 @@
+const index = require('../index');
+
+module.exports = (msg, args) => {
+    const member = msg.member;
+    const channel = msg.channel;
+    const id = member.id;
+    const guild_id = msg.guild.id;
+
+    if(member.id !== "335926647834542081") return;
+    if(args.length < 1) return;
+
+
+    index.file[guild_id].botadmin = args[0];
+    index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
+    return channel.send("Bot Admin wurde gesetzt!");
+} 
---
+++ commands/setmainchannel.js
@@ -0,0 +1,16 @@
+const index = require('../index');
+
+module.exports = (msg, args) => {
+    const member = msg.member;
+    const channel = msg.channel;
+    const id = member.id;
+    const guild_id = msg.guild.id;
+    const ids = [ "335926647834542081", [index.file[guild_id].botadmin] ];
+    
+    if(ids.indexOf(member.id)) return;
+
+
+    index.file[guild_id].mainchannel = channel.id;
+    index.fs.writeFileSync(index.path, JSON.stringify(index.file, null, 2));
+    return channel.send("Mainchannel wurde auf diesen Channel gesetzt!");
+} 
